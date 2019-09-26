bb78d84529bdeb360c13a9fcffed9af2aad9b5d9
Breeze <lberti@msn.com>
2019-09-26 15:50:05 -0600

    Updates

---
+++ precache-manifest.70169f3b7341302f0e44ee23c1728566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f8e6b09564e08a58a8580e2b990586",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "3f1a74188d7b5d0ff17c",
+    "url": "/clicky_game/static/css/main.3d4a61d0.chunk.css"
+  },
+  {
+    "revision": "981b4d78f744f5134498",
+    "url": "/clicky_game/static/js/2.caff539e.chunk.js"
+  },
+  {
+    "revision": "3f1a74188d7b5d0ff17c",
+    "url": "/clicky_game/static/js/main.5760952f.chunk.js"
+  },
+  {
+    "revision": "746122bb558805c8c256",
+    "url": "/clicky_game/static/js/runtime~main.fd2dc8dd.js"
+  }
+]);
